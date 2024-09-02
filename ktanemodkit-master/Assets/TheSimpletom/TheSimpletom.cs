000e3cc555a4fb6c43d377438153a5c03f031eef
Hawker <nyabentley@gmail.com>
2024-09-02 04:02:20 -0400

    add logic

--- ktanemodkit-master/Assets/TheSimpletom/TheSimpletom.cs
+++ ktanemodkit-master/Assets/TheSimpletom/TheSimpletom.cs
@@ -6,6 +6,7 @@ using System.Text.RegularExpressions;
 using UnityEngine;
 using KModkit;
 using Rnd = UnityEngine.Random;
+using UnityEditor;
 
 public class TheSimpletom : MonoBehaviour {
 
@@ -16,6 +17,12 @@ public class TheSimpletom : MonoBehaviour {
    static int ModuleIdCounter = 1;
    int ModuleId;
    private bool ModuleSolved;
+   private string buttonName;
+   private int buttonCount;
+   private int buttonPreses = 0;
+
+    [SerializeField]
+    KMSelectable button;
 
    void Awake () {
       ModuleId = ModuleIdCounter++;
@@ -24,11 +31,16 @@ public class TheSimpletom : MonoBehaviour {
             object.OnInteract += delegate () { keypadPress(object); return false; };
         }
         */
+        
 
-        //button.OnInteract += delegate () { buttonPress(); return false; };
+        button = transform.Find("The Everything/Button HL").GetComponent<KMSelectable>();
+
+        button.OnInteract += delegate () { ButtonPress(); return false; };
+        
+
         string[] labels = {"The Simpletom", "The Simpletim", "The Simplebob", "The Simplejack", "The Simplejane"};
         TextMesh textMesh = transform.Find("The Everything/Claw Text").GetComponent<TextMesh>();
-        string buttonName = labels[Rnd.Range(0, labels.Length)];
+        buttonName = labels[Rnd.Range(0, labels.Length)];
 
         if (debug)
         {
@@ -37,15 +49,54 @@ public class TheSimpletom : MonoBehaviour {
         textMesh.text = buttonName.Replace(" ", "\n");
     }
 
-   void Start () 
-   {
-        int indicatorCount = GetComponent<KMBombInfo>().GetIndicators().Count();
-        int buttonCount = 4 * indicatorCount - 3;
-        Debug.Log(buttonCount);
+    private void ButtonPress()
+    {
+        if (ModuleSolved)
+            return;
+
+        buttonPreses++;
+        Log($"The button was pressed {buttonPreses} times");
+        Solve();
+    }
+
+    void Start()
+    {
+        Log($"The button was labled {buttonName}");
+
+        switch (buttonName)
+        {
+            case "The Simpletom":
+                int indicatorCount = GetComponent<KMBombInfo>().GetIndicators().Count();
+                buttonCount = Modulo(4 * indicatorCount - 3, 4);
+                Log($"To solve you must press the button {buttonCount} times");
+                break;
+
+            default:
+                Debug.Log("Not Implemented");
+                break;
+        }
+
+        
    }
 
    void Update () {
         
+    }
+
+    public void Log(string s)
+    {
+        Debug.Log($"[The Simpletom #{ModuleId}] {s}");
+    }
+
+    private int Modulo(int value, int mod)
+    {
+        return ((value % mod) + mod) % mod;
+    }
+
+    private void Solve()
+    {
+        GetComponent<KMBombModule>().HandlePass();
+        ModuleSolved = true;
     }
 
 #pragma warning disable 414
