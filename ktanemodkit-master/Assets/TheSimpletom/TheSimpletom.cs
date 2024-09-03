1aa1d85fe7b009469515e25785fc7cf23cb5d778
Detective1883 <loganboddie644@gmail.com>
2024-09-02 17:56:38 -0700

    Morse Code Rule

--- ktanemodkit-master/Assets/TheSimpletom/TheSimpletom.cs
+++ ktanemodkit-master/Assets/TheSimpletom/TheSimpletom.cs
@@ -5,6 +5,8 @@ using KModkit;
 using Rnd = UnityEngine.Random;
 using NUnit.Framework;
 using System.Collections.Generic;
+using HarmonyLib;
+using System.Runtime.CompilerServices;
 
 public class TheSimpletom : MonoBehaviour
 {
@@ -13,17 +15,23 @@ public class TheSimpletom : MonoBehaviour
 
     static int ModuleIdCounter = 1;
     int ModuleId;
+    int dashordot;
+    int breaktimer;
+    int submittimer;
     private bool ModuleSolved;
+    private bool buttonHeld;
     private string buttonName;
     private int buttonCount;
     private int buttonPresses = 0;
     private float timer = 0;
     IEnumerator SBTimer;
     IEnumerator morseDashCoroutine;
-    const int morseDash = 2;
-    const int morseReset = 5;
-    const int morseBreak = 1;
-    List<string> morseValues;
+    const int morseDash = 100;
+    const int morseReset = 250;
+    const int morseBreak = 50;
+    const int morseSubmit = 100;
+    List<List<string>> morseValues;
+    private float inactiveTimer = 0;
 
     KMSelectable button;
     KMBombModule status;
@@ -50,7 +58,8 @@ public class TheSimpletom : MonoBehaviour
         TextMesh textMesh = transform.Find("The Everything/Claw Text").GetComponent<TextMesh>();
         buttonName = labels[Rnd.Range(0, labels.Length)];
 
-        morseValues = new List<string>();
+        morseValues = new List<List<string>>();
+        morseValues.Add(new List<string>());
 
         if (debug)
         {
@@ -58,13 +67,30 @@ public class TheSimpletom : MonoBehaviour
         }
         textMesh.text = buttonName.Replace(" ", "\n");
     }
-
+    private string PrintAllLists()
+    {
+        string input = "";
+        for (int i = 0; i < morseValues.Count; i++)
+        {
+            if (morseValues[i].Count == 0)
+            {
+                continue;
+            }
+            if (i != 0)
+            {
+                input += "/";
+            }
+                input += string.Join("", morseValues[i].ToArray());
+        }
+        return input;
+    }
     private void ButtonPress()
     {
         //when butt is presssed, set timer to 0 amd start timer
         //when timer == 3, if button presses is answer, solve mod, otherwise strike
         if (ModuleSolved)
             return;
+        buttonHeld = true;
         if (buttonName == "The Simpletom")
         {
             if (buttonPresses == 0)
@@ -80,13 +106,14 @@ public class TheSimpletom : MonoBehaviour
         }
         if (buttonName == "The Simplejack" || buttonName == "The Simplejill")
         {
-            morseDashCoroutine = MorseDashCoroutine();
-            StartCoroutine(morseDashCoroutine);
+            breaktimer = 0;
+            submittimer = 0;
         }
     }
 
     private void ButtonRelease()
     {
+        buttonHeld = false;
         if (buttonName == "The Simplebob")
         {
             StopCoroutine(SBTimer);
@@ -103,15 +130,17 @@ public class TheSimpletom : MonoBehaviour
         }
         if (buttonName == "The Simplejack" || buttonName == "The Simplejill")
         {
-            StopCoroutine(morseDashCoroutine);
-            Debug.Log(timer);
-            if (morseDash < timer)
+            int lastListIndex = morseValues.Count - 1;
+            List<string> lastList = morseValues[lastListIndex];
+            if (dashordot < morseDash)
             {
-                Debug.Log("dash");
-                morseValues[morseValues.Count - 1] = "dash";
+                lastList.Add(".");
             }
-            timer = 0;
-            Debug.Log(string.Join(" ", morseValues.ToArray()));
+            if (dashordot > morseDash)
+            {
+                lastList.Add("-");
+            }
+            dashordot = 0;
         }
     }
 
@@ -131,7 +160,7 @@ public class TheSimpletom : MonoBehaviour
                 break;
             case "The Simplejack":
             case "The Simplejill":
-                Log($"To solve you must submit 'HILL' in Morse Code");
+                Log($"To solve you must submit 'HILL' in Morse Code, or ..../../.-../.-..");
                 break;
             default:
                 Debug.Log("Not Implemented");
@@ -140,10 +169,56 @@ public class TheSimpletom : MonoBehaviour
 
 
     }
-
+    private void FixedUpdate()
+    {
+        if (ModuleSolved)
+            return;
+        int lastListIndex = morseValues.Count - 1;
+        List<string> lastList = morseValues[lastListIndex];
+        List<string> firstList = morseValues[0];
+        if (buttonHeld)
+        {
+            dashordot++;
+            if (dashordot == morseDash)
+            {
+                Debug.Log("dash sound effect");
+            }
+            if (dashordot == morseReset)
+            {
+                Debug.Log("reset sound effect");
+            }
+        }
+        else if (lastList.Count > 0)
+        {
+            breaktimer++;
+            if (breaktimer == morseBreak)
+            {
+                Debug.Log("break sound effect");
+                PrintAllLists();
+                morseValues.Add(new List<string>());
+            }
+        }
+        else if (firstList.Count > 0)
+        {
+            submittimer++;
+            if (submittimer == morseSubmit)
+            {
+                string morseInput = PrintAllLists();
+                Log($"You inputted {morseInput}");
+                if (morseInput == "..../../.-../.-..")
+                {
+                    Solve();
+                }
+                else
+                {
+                    status.HandleStrike();
+                }
+            }
+        }
+    }
     void Update()
     {
-
+ 
     }
 
 
@@ -188,16 +263,6 @@ public class TheSimpletom : MonoBehaviour
             yield return null;
             timer += Time.deltaTime;
         }
-    }
-    private IEnumerator MorseDashCoroutine()
-    {
-        morseValues.Add("dot");
-        while(true)
-        {
-            yield return null;
-            timer += Time.deltaTime;
-        }
-
     }
 
 #pragma warning disable 414
